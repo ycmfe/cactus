9f5c2bc2a7d0d347f8bc2af7363756ce1a00b8eb
chunyuan <chunyuan@guanghe.tv>
2021-05-11 11:15:29 +0800

    feat: addPlugin actus-points

--- template/index.js
+++ template/index.js
@@ -26,6 +26,9 @@ exports.getPreset = function getPreset(options) {
     .addPlugin('@ycmfe/vue-cli-plugin-cactus-vw', {}, () => {
       return options.toViewport
     })
+    .addPlugin('@ycmfe/vue-cli-plugin-cactus-points', {}, () => {
+      return options.buryPoints
+    })
 
   return preset.toJsonStr()
 }
